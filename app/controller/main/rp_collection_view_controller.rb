f6e75d3463c046492ca7cae42041f18ff3e676a8
Torben <toepper@redrauscher.de>
2014-01-24 10:32:42 +0100

    Adds missing RemoteRequestHandler

--- app/controller/main/rp_collection_view_controller.rb
+++ app/controller/main/rp_collection_view_controller.rb
@@ -1,5 +1,11 @@
 class RPCollectionViewController < UICollectionViewController
   include Helper::Authentication
+  include Modules::RemoteRequestHandler
+
+  def viewWillDisappear(animated)
+    super
+    handleViewWillDisappear
+  end
 
   def prefersStatusBarHidden
     true
--- app/controller/main/rp_table_view_controller.rb
+++ app/controller/main/rp_table_view_controller.rb
@@ -1,10 +1,16 @@
 class RPTableViewController < UITableViewController
   include Helper::Authentication
+  include Modules::RemoteRequestHandler
 
   def viewDidLoad
     self.edgesForExtendedLayout = UIRectEdgeTop
 
     super
+  end
+
+  def viewWillDisappear(animated)
+    super
+    handleViewWillDisappear
   end
 
   def prefersStatusBarHidden
